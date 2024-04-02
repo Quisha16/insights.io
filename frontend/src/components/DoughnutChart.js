a955fa752708b00452add55f458e5009ae6f4c3e
lizzencamelo <lizzencamelo02@gmail.com>
2024-04-02 15:24:10 +0530

    Uploading backend files.

--- frontend/src/components/DoughnutChart.js
+++ frontend/src/components/DoughnutChart.js
@@ -8,44 +8,54 @@ const DoughnutChart = () => {
   const chartInstance = useRef(null);
 
   useEffect(() => {
-    const ctx = chartRef.current.getContext('2d');
-    
-    if (chartInstance.current !== null) {
-      chartInstance.current.destroy(); // Destroy the previous Chart instance
-    }
+    fetchData();
+  }, []);
 
-    chartInstance.current = new Chart(ctx, {
-      type: 'doughnut', 
-      data: {
-        labels: ['Positive', 'Negative'],
-        datasets: [{
-          label: 'Reviews',
-          data: [70, 30], // Hardcoded values for positive and negative reviews
-          backgroundColor: [
-            '#ffc872', // Yellow-positive reviews
-            '#006bbb', // Blue-negative reviews
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        responsive: true,
-        plugins: {
-          legend: {
-            labels: {
-              color: '##070F2B' // Set legend label color
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/sentiment_prediction/');
+      const responseData = await response.json();
+      const [labels, data]=[Object.keys(responseData.prediction_data), Object.values(responseData.prediction_data)];
+
+      const ctx = chartRef.current.getContext('2d');
+      if (chartInstance.current !== null) {
+        chartInstance.current.destroy(); // Destroy the previous Chart instance
+      }
+      chartInstance.current = new Chart(ctx, {
+        type: 'doughnut', 
+        data: {
+          labels: labels,
+          datasets: [{
+            label: 'Reviews',
+            data: data,
+            backgroundColor: [
+              '#ffc872', // Yellow-positive reviews
+              '#006bbb', // Blue-negative reviews
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              labels: {
+                color: '##070F2B' // Set legend label color
+              }
             }
           }
         }
-      }
-    });
+      });
 
-    return () => {
-      if (chartInstance.current !== null) {
-        chartInstance.current.destroy(); // Cleanup on component unmount
-      }
-    };
-  }, []);
+      // return () => {
+      //   if (chartInstance.current !== null) {
+      //     chartInstance.current.destroy(); // Cleanup on component unmount
+      //   }
+      //};
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
   return (
     <div>
