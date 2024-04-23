87c7af53298d06c580d88ff5a30b579e2099db8d
Quisha16 <quishacoutinho@gmail.com>
2024-04-23 20:25:40 +0530

    frontend update

--- frontend/src/components/MainContent/MainContent.jsx
+++ frontend/src/components/MainContent/MainContent.jsx
@@ -29,3 +29,48 @@ const MainContent = () => {
 }
 
 export default MainContent;
+
+
+// import React from 'react';
+// import Carousel from 'react-bootstrap/Carousel';
+// import DoughnutChart from '../Charts/DoughnutChart';
+// import PolarAreaChart from '../Charts/PolarAreaChart';
+// import LineGraph from '../Charts/LineGraph'; 
+// import StackedBarChart from '../Charts/StackedBarChart'; 
+// import HorizontalBarChart from '../Charts/HorizontalBarChart';
+// import LineGraph2 from '../Charts/LineGraph2';
+// import WordCloud from '../Charts/wordCloud'; // Import the WordCloudComponent
+
+// const MainContent = () => {
+//   return (
+//     <Carousel>
+//       <Carousel.Item>
+//         <div>
+//           <PolarAreaChart />
+//           <LineGraph/>
+//           <StackedBarChart/>
+//         </div>
+//       </Carousel.Item>
+//       <Carousel.Item>
+//         <div>
+//           <DoughnutChart />
+//           <HorizontalBarChart/>
+//           <LineGraph2/>
+//         </div>
+//       </Carousel.Item>
+//       <Carousel.Item>
+//         <div>
+//           <WordCloud /> 
+//         </div>
+//       </Carousel.Item>
+//     </Carousel>
+//   );
+// }
+
+// export default MainContent;
+
+
+
+
+
+
