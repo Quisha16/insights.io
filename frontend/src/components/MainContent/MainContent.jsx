c99af083942bb8ce213c6b8e1832aa4220417fe3
Quisha16 <quishacoutinho@gmail.com>
2024-05-02 21:49:12 +0530

    labels added

--- frontend/src/components/MainContent/MainContent.jsx
+++ frontend/src/components/MainContent/MainContent.jsx
@@ -12,13 +12,35 @@ const MainContent = () => {
     <div className="maincontent">
         <h1 className='title'>Dashboard</h1>
       <div className="chartlayout">
+      <div className="blurry-box">
+          <h5>Customer Ratings</h5>
             <PolarAreaChart />
-            <LineGraph/>
-            <StackedBarChart/>
+          </div>
+          <div className="blurry-box">
+            <h5>Product Performance Over Months</h5>
+            <LineGraph />
+          </div>
+          <div className="blurry-box">
+            <h5>Ratio of Positive and Negative Sentiments Overtime</h5>
+            <StackedBarChart />
+          </div>
+            <div className="blurry-box">
+          <h5>Customer Sentiment</h5>
             <DoughnutChart />
-            <HorizontalBarChart/>
-            <LineGraph2/>
-            <WordCloud /> 
+          </div>
+          <div className="blurry-box">
+            <h5>Areas Of improvement</h5>
+            <HorizontalBarChart />
+          </div>
+          <div className="blurry-box">
+          <h5>Positive Sentiment Over Time</h5>
+            <LineGraph2 />
+          </div>
+          <div className="blurry-box">
+          <h5>Sentiment Word Cloud</h5>
+          <WordCloud /> 
+          </div>
+          
             </div>  
              
       </div>
